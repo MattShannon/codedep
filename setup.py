48648b1f275380ac1cbe52d234f331a2aca409ac
Matt Shannon <matt.shannon@cantab.net>
2013-07-18 00:45:52 +0100

    add stuff for distributing package using distutils
    Ignore-this: aeb02fba3b32552fb8611b1aa29b01d3
    
    darcs-hash:20130717234552-3a4db-8f48c3b47a761ad92dffd00cb17073daf61e32d3

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""A distutils-based script for distributing and installing codedep."""
+
+# Copyright 2011, 2012, 2013 Matt Shannon
+
+# This file is part of codedep.
+# See `License` for details of license and warranty.
+
+
+from distutils.core import setup
+
+with open('README.rst') as readmeFile:
+    long_description = readmeFile.read()
+
+setup(
+    name = 'codedep',
+    version = '0.2.dev1',
+    description = 'Semi-automated code-level dependency tracking for python.',
+    url = 'http://github.com/MattShannon/codedep',
+    author = 'Matt Shannon',
+    author_email = 'matt.shannon@cantab.net',
+    license = '3-clause BSD (see License file)',
+    packages = ['codedep'],
+    scripts = ['bin/codedep_check'],
+    long_description = long_description,
+)
