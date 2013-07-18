c07903fa9afcca77063748e7482ce08a832a69cb
Matt Shannon <matt.shannon@cantab.net>
2013-07-18 01:48:51 +0100

    start development of 0.3
    Ignore-this: 8f19a4df0fb73354d600f2c9c739b90c
    
    darcs-hash:20130718004851-3a4db-922b1a02596541e7af87ad644814def8e16e7b62

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst') as readmeFile:
 
 setup(
     name = 'codedep',
-    version = '0.2',
+    version = '0.3.dev1',
     description = 'Semi-automated code-level dependency tracking for python.',
     url = 'http://github.com/MattShannon/codedep',
     author = 'Matt Shannon',
